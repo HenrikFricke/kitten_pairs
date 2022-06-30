4665db9228e96abe6c943b9e51102fa8442f1d6d
Henrik Fricke <henrikfricke@web.de>
2022-06-30 16:52:28 +0200

    test: game context (#2)

---
+++ test/kitten_pairs/game_test.exs
@@ -0,0 +1,40 @@
+defmodule KittenPairs.GameTest do
+  use KittenPairs.DataCase
+
+  alias KittenPairs.Game
+
+  describe "create_game/1" do
+    test "creates a game and player" do
+      {:ok, game, player} = Game.create_game("Chi")
+
+      assert Repo.exists?(from g in Game.Game, where: g.id == ^game.id)
+
+      assert Repo.exists?(
+               from p in Game.Player,
+                 where: p.id == ^player.id and p.game_id == ^game.id and p.is_navigator == true
+             )
+    end
+  end
+
+  describe "join_game/2" do
+    test "creates a player" do
+      {:ok, game} = Repo.insert(%Game.Game{})
+      {:ok, player} = Game.join_game(game.id, "Chi")
+
+      assert Repo.exists?(
+               from p in Game.Player,
+                 where: p.id == ^player.id and p.game_id == ^game.id and p.is_navigator == false
+             )
+    end
+  end
+
+  describe "get_game_by_id/1" do
+    test "returns a game" do
+      {:ok, g} = Repo.insert(%Game.Game{})
+      game = Game.get_game_by_id(g.id)
+
+      assert game.id == g.id
+      assert game.players == []
+    end
+  end
+end
