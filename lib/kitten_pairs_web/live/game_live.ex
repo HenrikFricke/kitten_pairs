753ad6dfcd10aefb27ecb4e0e03eb219ad974e12
Henrik Fricke <henrikfricke@web.de>
2022-07-04 00:14:33 +0200

    feat(game live): create round / render round view

--- lib/kitten_pairs_web/live/game_live.ex
+++ lib/kitten_pairs_web/live/game_live.ex
@@ -13,11 +13,30 @@ defmodule KittenPairsWeb.GameLive do
         {:ok, redirect(socket, to: Routes.startpage_path(socket, :index))}
 
       game ->
+        last_round = Game.get_last_round(game.id)
         current_player = Enum.find(game.players, fn p -> p.id == player_id end)
         join_link = Routes.startpage_url(socket, :index, game.id)
         Game.notify(game_id, current_player.id, [:player, :joined])
 
-        {:ok, assign(socket, game: game, current_player: current_player, join_link: join_link)}
+        {:ok,
+         assign(socket,
+           game: game,
+           current_player: current_player,
+           join_link: join_link,
+           last_round: last_round
+         )}
+    end
+  end
+
+  def handle_event("create_round", _value, socket) do
+    game_id = socket.assigns.game.id
+    current_player_id = socket.assigns.current_player.id
+
+    case Game.create_round(game_id) do
+      {:ok, round} ->
+        Game.notify(game_id, current_player_id, [:round, :created])
+
+        {:noreply, assign(socket, :last_round, round)}
     end
   end
 
@@ -30,6 +49,15 @@ defmodule KittenPairsWeb.GameLive do
       when not current_player?(socket, player_id) do
     game_id = socket.assigns.game.id
     game = Game.get_game_by_id(game_id)
+
     {:noreply, assign(socket, :game, game)}
+  end
+
+  def handle_info({[:round, :created], player_id, _}, socket)
+      when not current_player?(socket, player_id) do
+    game_id = socket.assigns.game.id
+    last_round = Game.get_last_round(game_id)
+
+    {:noreply, assign(socket, :last_round, last_round)}
   end
 end
--- test/kitten_pairs_web/live/game_live_test.exs
+++ test/kitten_pairs_web/live/game_live_test.exs
@@ -11,16 +11,42 @@ defmodule KittenPairsWeb.GameLiveTest do
     %{conn: conn, game: game, player: player}
   end
 
-  test "waiting for other to join", %{conn: conn, game: game} do
-    {:ok, _view, html} = live(conn, "/games/#{game.id}")
+  describe "game overview" do
+    test "waiting for other to join", %{conn: conn, game: game} do
+      {:ok, _view, html} = live(conn, "/games/#{game.id}")
 
-    assert html =~ "Share this link with a friend to start the first round:"
-    assert html =~ "http://localhost:4002/join/#{game.id}"
+      assert html =~ "Share this link with a friend to start the first round:"
+      assert html =~ "http://localhost:4002/join/#{game.id}"
+    end
+
+    test "round creation", %{conn: conn, game: game} do
+      {:ok, _player} = Game.join_game(game.id, "Hen")
+      {:ok, view, _html} = live(conn, "/games/#{game.id}")
+
+      assert view
+             |> element("button#create_round")
+             |> render_click() =~ "round test"
+    end
+
+    test "unknown game id", %{conn: conn} do
+      path = Routes.startpage_path(conn, :index)
+
+      assert live(conn, "/games/abc") == {:error, {:redirect, %{flash: %{}, to: path}}}
+    end
   end
 
-  test "unknown game id", %{conn: conn} do
-    path = Routes.startpage_path(conn, :index)
+  describe "round" do
+    setup %{conn: conn, game: game} do
+      {:ok, _player} = Game.join_game(game.id, "Hen")
+      {:ok, _round} = Game.create_round(game.id)
 
-    assert live(conn, "/games/abc") == {:error, {:redirect, %{flash: %{}, to: path}}}
+      %{conn: conn, game: game}
+    end
+
+    test "initial render", %{conn: conn, game: game} do
+      {:ok, _view, html} = live(conn, "/games/#{game.id}")
+
+      assert html =~ "round test"
+    end
   end
 end
