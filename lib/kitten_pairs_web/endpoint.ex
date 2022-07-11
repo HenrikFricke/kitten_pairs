59c61cd4c40b6a00a099a9d27572dc83ba33b652
Henrik Fricke <henrikfricke@web.de>
2022-07-11 20:34:22 +0200

    fix: define manifest.json as static asset

--- lib/kitten_pairs_web/endpoint.ex
+++ lib/kitten_pairs_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule KittenPairsWeb.Endpoint do
     at: "/",
     from: :kitten_pairs,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt manifest.json)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
